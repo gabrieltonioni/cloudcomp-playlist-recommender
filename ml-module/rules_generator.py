57b8bdad7c28239ebbe285976357f36660adb3b0
Gabriel Tonioni Duarte <gtonioni13@gmail.com>
2023-11-28 18:44:54 -0300

    Bumped gabrieltonioni/rules-generator to 0.0.2

--- ml-module/rules_generator.py
+++ ml-module/rules_generator.py
@@ -8,19 +8,19 @@ import pickle
 
 def main():
     ssl._create_default_https_context = ssl._create_unverified_context
-    url = os.environ['DS_URL']
+    url = os.environ["DS_URL"]
     playlists = pd.read_csv(url)
 
     # One-hot encoding of playlists dataframe
-    playlists['value'] = 1
-    playlists = playlists.pivot_table(index='pid', columns='track_uri', values='value', fill_value=0)
+    playlists["value"] = 1
+    playlists = playlists.pivot_table(index="pid", columns="track_uri", values="value", fill_value=0)
     playlists = playlists.astype(bool)
 
     # Generates a dataframe to store association rules
     frequent_itemsets = apriori(playlists, min_support=0.07, use_colnames=True)
-    rules = association_rules(frequent_itemsets, metric='lift', min_threshold=1)
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
 
-    with open('/ml-data/rules.pickle', 'wb') as f:
+    with open("/ml-data/rules.pickle", "wb") as f:
         pickle.dump(rules, f)
 
 
